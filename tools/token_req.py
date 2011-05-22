21e79d4cd3012d7aaba238720c9d673da2f5ea3c
Marcin Sztolcman <marcin@urzenia.net>
2011-05-22 21:47:46 +0200

    fixes for 3.x
    - use urllib insteaad of urllib2
    - better handling of app_data file with tokens
    - fix print() calls

--- tools/token_req.py
+++ tools/token_req.py
@@ -9,37 +9,35 @@ CONSUMER_KEY        = ''
 CONSUMER_SECRET     = ''
 
 from oauth import oauth
-import urllib2
+import urllib
 import sys
 import os
 
 with open (os.path.join (os.path.dirname (sys.argv[0]), 'app_data'), 'r') as fh:
     for line in fh:
         line = line.split ('=')
-        line[1] = '"' + line[1].strip () + '"'
-        line = '='.join (line)
-        eval (line)
+        globals()[line[0].strip()] = line[1]
 
 consumer = oauth.OAuthConsumer (CONSUMER_KEY, CONSUMER_SECRET)
 oauth_request = oauth.OAuthRequest.from_consumer_and_token (consumer, http_url=URL_REQUEST_TOKEN)
 signature = oauth.OAuthSignatureMethod_HMAC_SHA1 ()
 oauth_request.sign_request(signature, consumer, None)
 
-print 'Connecting to: %s' % oauth_request.to_url ()
+print ('Connecting to: %s' % oauth_request.to_url ())
 req = urllib2.Request (oauth_request.to_url ())
 
 opener = urllib2.build_opener ()
 data = opener.open (req)
 resp = data.read ()
-print resp
+print (resp)
 
 token = oauth.OAuthToken.from_string (resp)
-print 'REQUEST TOKEN:', token
+print ('REQUEST TOKEN:', token)
 
 oauth_request = oauth.OAuthRequest.from_token_and_callback (token = token, http_url = URL_AUTHORIZE)
-print "Visit this URL and accept access:\n%s\n" % oauth_request.to_url ()
+print ("Visit this URL and accept access:\n%s\n" % oauth_request.to_url ())
 
-print "Now enter PIN code from blip.pl: "
+print ("Now enter PIN code from blip.pl: ")
 oauth_verifier = raw_input ()
 oauth_request = oauth.OAuthRequest.from_consumer_and_token (consumer, token=token, verifier=oauth_verifier, http_url = URL_ACCESS_TOKEN)
 oauth_request.sign_request (signature, consumer, token)
@@ -48,8 +46,8 @@ req = urllib2.Request (oauth_request.to_url ())
 data = opener.open (req)
 access_token = oauth.OAuthToken.from_string (data.read ())
 
-print "OAuath TOKEN: ", access_token
-print "OAuath SECRET: ", access_token
+print ("OAuath TOKEN: ", access_token)
+print ("OAuath SECRET: ", access_token)
 
-print "Full response:\n", access_token
+print ("Full response:\n", access_token)
 
