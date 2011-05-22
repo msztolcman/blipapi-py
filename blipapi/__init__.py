d4a0a7f1b1648603458053f2627018a1f7062c86
Marcin Sztolcman <marcin@urzenia.net>
2011-05-22 21:45:37 +0200

    relative imports

--- blipapi/__init__.py
+++ blipapi/__init__.py
@@ -6,6 +6,6 @@
 # Copyright: (r) 2009 Marcin Sztolcman
 # License: http://opensource.org/licenses/lgpl-3.0.html The GNU Lesser General Public License, version 3.0 (LGPLv3)
 
-from blipapi import BlipApi
-from blipapi import BlipApiError
+from .blipapi import BlipApi
+from .blipapi import BlipApiError
 
